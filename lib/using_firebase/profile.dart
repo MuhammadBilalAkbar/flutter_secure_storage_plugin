bfca8cbc8e4c87d2cdfa883668459a3ebdbd522e
Muhammad Bilal Akbar <mbilalakbarkh786@gmail.com>
2023-02-05 12:43:23 +0500

    Initial commit

---
+++ lib/using_firebase/profile.dart
@@ -0,0 +1,98 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+import 'login.dart';
+
+class Profile extends StatefulWidget {
+  Profile({Key? key}) : super(key: key);
+
+  @override
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  final uid = FirebaseAuth.instance.currentUser!.uid;
+  final email = FirebaseAuth.instance.currentUser!.email;
+  final creationTime = FirebaseAuth.instance.currentUser!.metadata.creationTime;
+
+  User? user = FirebaseAuth.instance.currentUser;
+
+  final storage = const FlutterSecureStorage();
+
+  verifyEmail() async {
+    if (user != null && !user!.emailVerified) {
+      await user!.sendEmailVerification();
+      print('Verification Email has been sent');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          backgroundColor: Colors.orangeAccent,
+          content: Text(
+            'Verification Email has been sent',
+            style: TextStyle(fontSize: 18.0, color: Colors.black),
+          ),
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("Welcome User"),
+            ElevatedButton(
+              onPressed: () async => {
+                await FirebaseAuth.instance.signOut(),
+                await storage.delete(key: "uid"),
+                Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Login(),
+                    ),
+                    (route) => false)
+              },
+              child: Text('Logout'),
+              style: ElevatedButton.styleFrom(primary: Colors.blueGrey),
+            )
+          ],
+        ),
+      ),
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        child: Column(
+          children: [
+            Text(
+              'User ID: $uid',
+              style: TextStyle(fontSize: 18.0),
+            ),
+            Row(
+              children: [
+                Text(
+                  'Email: $email',
+                  style: TextStyle(fontSize: 18.0),
+                ),
+                user!.emailVerified
+                    ? Text(
+                        'verified',
+                        style:
+                            TextStyle(fontSize: 18.0, color: Colors.blueGrey),
+                      )
+                    : TextButton(
+                        onPressed: () => {verifyEmail()},
+                        child: Text('Verify Email'))
+              ],
+            ),
+            Text(
+              'Created: $creationTime',
+              style: TextStyle(fontSize: 18.0),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
