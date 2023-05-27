3224edaf2d1248a3722edc01048e8a3a3c220e0b
MuhammadBilalAkbar <mbilalakbarkh786@gmail.com>
2023-05-27 18:05:41 +0500

    added more functionality with remember me

--- lib/using_firebase/login.dart
+++ lib/using_firebase/login.dart
@@ -1,9 +1,10 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
 import 'profile.dart';
 import 'forgot_password.dart';
 import 'signup.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key, this.email = '', this.password = ''})
